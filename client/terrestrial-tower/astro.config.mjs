4aaf5c1ca5f3879bebef9c13896b891cdc757400
Your Name <you@example.com>
2024-10-29 19:08:56 +0500

    initial commit

---
+++ client/terrestrial-tower/astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import lottie from "astro-integration-lottie";
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), lottie()]
+});
